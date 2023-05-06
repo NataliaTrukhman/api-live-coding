8bf548832aa2b8ae44eec06e12ec653181f30500
natalia-trukhman <119609124+natalia-trukhman@users.noreply.github.com>
2023-05-06 15:23:22 +0300

    добавляем форму регистрации

--- components/login-component.js
+++ components/login-component.js
@@ -1,12 +1,23 @@
 import { loginUser } from "../api.js";
 
 export function renderLoginComponent({ appElement, setToken, fetchTodosAndRender }) {   //рендерим форму входа
-    const appHtml = ` 
+    let isLoginMode = true   //то показываем форму логина. если false - форму регистрации
+
+
+    const renderForm = () => {     //после каждого переключения кнопки toggle заново отрендерить всю разметку
+
+        const appHtml = ` 
                 <h1>Список задач</h1>
 
                 <div class="form">
-                    <h3 class="form-title">Форма входа</h3>
+                    <h3 class="form-title">Форма ${isLoginMode ? 'входа' : 'регистрации'}</h3>
                     <div class="form-row">
+                    ${isLoginMode ? ''
+                :
+                `Имя:
+                        <input type="text" id="name-input" class="input"/>
+                         <br/>`}
+                       
                         Логин:
                         <input type="text" id="login-input" class="input"/>
                         <br/>
@@ -14,34 +25,46 @@ export function renderLoginComponent({ appElement, setToken, fetchTodosAndRender
                         <input type="password" id="password-input" class="input"/>
                     </div>
                     <br/>
-                    <button class="button" id="login-button">Войти</button>
+                    <button class="button" id="login-button">${isLoginMode ? 'Войти' : 'Зарегистрироваться'}</button>
+                       <br/>
+                          <br/>
+                     <button class="button" id="toggle-button">Перейти ${isLoginMode ? 'к регистрации' : 'ко входу'}</button>
                 </div>`;
 
-    appElement.innerHTML = appHtml; //рендерим приложение 
+        appElement.innerHTML = appHtml; //рендерим приложение 
 
-    document.getElementById("login-button").addEventListener('click', () => {
-        const login = document.getElementById("login-input").value; //достаем значение
-        const password = document.getElementById("password-input").value;
-        //валидация 
-        if (!login) {
-            alert("Введите логин!")
-            return;
-        }
-        if (!password) {
-            alert("Введите пароль!")
-            return;
-        }
-        loginUser({
-            login: login,
-            password: password,
-        }).then((user) => {
-            setToken(`Bearer ${user.user.token}`); //
-            fetchTodosAndRender();
-        }).catch(error=>{
-            //ToDo выводить alert красиво
-            alert(error.message)
-        })
+        document.getElementById("login-button").addEventListener('click', () => {
+            const login = document.getElementById("login-input").value; //достаем значение
+            const password = document.getElementById("password-input").value;
+            //валидация 
+            if (!login) {
+                alert("Введите логин!")
+                return;
+            }
+            if (!password) {
+                alert("Введите пароль!")
+                return;
+            }
+            loginUser({
+                login: login,
+                password: password,
+            }).then((user) => {
+                setToken(`Bearer ${user.user.token}`); //
+                fetchTodosAndRender();
+            }).catch(error => {
+                //ToDo выводить alert красиво
+                alert(error.message)
+            })
 
-        //перендерим приложение чтобы отрендерился список/ запрашиваем данные через fetch
-    });
+            //перендерим приложение чтобы отрендерился список/ запрашиваем данные через fetch
+        });
+
+        document.getElementById("toggle-button").addEventListener('click', () => {
+            isLoginMode = !isLoginMode;  // переключаем на противоположное направление
+            renderForm();
+        });
+    };
+
+    renderForm();
+
 }
