0c245c4fdcc87e6dd4087f082b80b29fd605940f
natalia-trukhman <119609124+natalia-trukhman@users.noreply.github.com>
2023-05-06 14:30:41 +0300

    Оживляет форму регистрации

--- components/login-component.js
+++ components/login-component.js
@@ -1,4 +1,4 @@
-import { login } from "../api.js";
+import { loginUser } from "../api.js";
 
 export function renderLoginComponent({ appElement, setToken, fetchTodosAndRender }) {   //рендерим форму входа
     const appHtml = ` 
@@ -11,7 +11,7 @@ export function renderLoginComponent({ appElement, setToken, fetchTodosAndRender
                         <input type="text" id="login-input" class="input"/>
                         <br/>
                         Пароль:
-                        <input type="text" id="login-input" class="input"/>
+                        <input type="password" id="password-input" class="input"/>
                     </div>
                     <br/>
                     <button class="button" id="login-button">Войти</button>
@@ -20,15 +20,28 @@ export function renderLoginComponent({ appElement, setToken, fetchTodosAndRender
     appElement.innerHTML = appHtml; //рендерим приложение 
 
     document.getElementById("login-button").addEventListener('click', () => {
-       
-        login({
-            login: "admin",
-            password: "admin",
-        }).then((user)=>{
+        const login = document.getElementById("login-input").value; //достаем значение
+        const password = document.getElementById("password-input").value;
+        //валидация 
+        if (!login) {
+            alert("Введите логин!")
+            return;
+        }
+        if (!password) {
+            alert("Введите пароль!")
+            return;
+        }
+        loginUser({
+            login: login,
+            password: password,
+        }).then((user) => {
             setToken(`Bearer ${user.user.token}`); //
             fetchTodosAndRender();
-        });
+        }).catch(error=>{
+            //ToDo выводить alert красиво
+            alert(error.message)
+        })
 
-         //перендерим приложение чтобы отрендерился список/ запрашиваем данные через fetch
-    }); 
+        //перендерим приложение чтобы отрендерился список/ запрашиваем данные через fetch
+    });
 }
